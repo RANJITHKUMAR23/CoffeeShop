e28df93aca0f075ecd48ccea3b5bae3d05eb1250
RANJITHKUMAR23 <ranjithsri.civil23@gmail.com>
2019-01-24 11:15:32 +0530

    Added company map

---
+++ CoffeeShop/CoffeeShop.Models/Map/CompanyMap.cs
@@ -0,0 +1,29 @@
+﻿using CoffeeShop.Models.Models;
+using System.Data.Entity.ModelConfiguration;
+
+namespace CoffeeShop.Models.Map
+{
+    public class CompanyMap: EntityTypeConfiguration<Company>
+    {
+        public CompanyMap()
+        {
+            HasKey(m => m.Id);
+            Property(m => m.Name).IsRequired().HasMaxLength(500);
+            Property(m => m.AliasName).IsOptional().HasMaxLength(500);
+            Property(m => m.Address).IsOptional().HasMaxLength(1000);
+            Property(m => m.City).IsRequired().HasMaxLength(200);
+            Property(m => m.StateId).IsRequired();
+            Property(m => m.CountryId).IsRequired();
+            Property(m => m.Zipcode).IsRequired();
+            Property(m => m.CurrencyId).IsRequired();
+            Property(m => m.PhoneNumber).IsRequired();
+            Property(m => m.MobileNumber).IsRequired();
+            Property(m => m.GstNo).IsRequired();
+            Property(m => m.Website).IsOptional().HasMaxLength(500);
+            Property(m => m.Logo).IsOptional().HasMaxLength(4000);
+            HasRequired(m => m.CreatedApplicationUser).WithMany().HasForeignKey(m => m.CreatedBy).WillCascadeOnDelete(false);
+            HasOptional(m => m.UpdatedApplicationUser).WithMany().HasForeignKey(m => m.UpdatedBy).WillCascadeOnDelete(false);
+
+        }
+    }
+}
